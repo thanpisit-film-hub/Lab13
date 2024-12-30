38b7eb5e9565936d1c4160b4f5a8807fc21fc784
thanpisit-film-hub <tanpisit.2548@gmail.com>
2024-12-31 02:39:27 +0700

    Lab13

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,33 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[N][M],int s,int x,int y){
+      for(int i = 0; i < N; i++){
+          for(int j = 0; j < M; j++){
+              if(sqrt(pow(i - x, 2) + pow(j - y, 2)) <= s - 1) image[i][j] = true;
+          }
+      }
+}
+
+void showImage(const bool image[N][M]){
+    cout << "-";
+    for(int i = 0; i < M; i++){
+        cout << "-";   
+    }
+    cout << "-\n";
+    
+    for(int j = 0;j < N; j++){
+        cout << "|";
+        for(int k = 0; k < M; k++){
+            if(image[j][k]) cout << "*";
+            else cout << " ";
+        }
+        cout << "|\n";
+    }
+    cout << "-";
+    for(int Q = 0; Q < M; Q++){
+        cout << "-";   
+    }
+    cout << "-\n";
+}
